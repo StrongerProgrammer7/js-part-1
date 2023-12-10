34d040d6bef411acfa67952a43859f3bb10060d1
Abdyukov Zufar <swat55551@gmail.com>
2023-12-10 11:51:59 +0300

    Transalte blackBoxJS Added: Interface INameCountry ICountry methods: getDataAsync getDataPromise loadCountriesData

--- blackBox.ts
+++ blackBox.ts
@@ -1,14 +1,27 @@
-/* eslint-disable no-restricted-imports */
-/* eslint-disable curly */
-/* eslint-disable prettier/prettier */
-// @ts-nocheck
 import module from './maps.js';
-let totalRequest = 0;
 
-async function getDataAsync(url)
+type CustomError = { status: number, customError: string};
+
+interface INameCountry
+{
+    common:string;
+    nativeName: Object;
+}
+
+interface ICountry
+{
+    area:number;
+    cca3:string;
+    name?: INameCountry;
+    borders?: Array<string>;
+}
+
+let totalRequest: number = 0;
+
+async function getDataAsync(url:string) :Promise<ICountry[]>
 {
     // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-    const response = await fetch(url,
+    const response: Response = await fetch(url,
     {
         method: 'GET',
         headers:
@@ -29,8 +42,10 @@ async function getDataAsync(url)
     //     throw error;
     // }
 
+
     // Если мы тут, значит, запрос выполнился.
     // Но там может быть 404, 500, и т.д., поэтому проверяем ответ.
+
     if (response.ok)
     {
         return response.json();
@@ -39,7 +54,7 @@ async function getDataAsync(url)
     // Пример кастомной ошибки (если нужно проставить какие-то поля
     // для внешнего кода). Можно выкинуть и сам `response`, смотря
     // какой у вас контракт. Главное перевести код в ветку `catch`.
-    const error =
+    const error:CustomError =
     {
         status: response.status,
         customError: 'wtfAsync',
@@ -48,7 +63,7 @@ async function getDataAsync(url)
 }
 
 // Загрузка данных через промисы (то же самое что `getDataAsync`)
-function getDataPromise(url)
+function getDataPromise(url:string) :Promise<ICountry[]>
 {
     // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
     return fetch(url,
@@ -70,17 +85,19 @@ function getDataPromise(url)
             // Пример кастомной ошибки (если нужно проставить какие-то поля
             // для внешнего кода). Можно зареджектить и сам `response`, смотря
             // какой у вас контракт. Главное перевести код в ветку `catch`.
-            return Promise.reject({
+            const error:CustomError =
+            {
                 status: response.status,
                 customError: 'wtfPromise',
-            });
+            };
+            return Promise.reject(error);
         },
 
         // При сетевой ошибке (мы оффлайн) из fetch вылетит эксцепшн,
         // и мы попадём в `onRejected` или в `.catch()` на промисе.
         // Если не добавить `onRejected` или `catch`, при ошибке будет
         // эксцепшн `Uncaught (in promise)`.
-        (error) =>
+        (error:unknown) =>
         {
             // Если не вернуть `Promise.reject()`, для внешнего кода
             // промис будет зарезолвлен с `undefined`, и мы не попадём
@@ -91,134 +108,25 @@ function getDataPromise(url)
         }
     );
 }
-
-// Две функции просто для примера, выберите с await или promise, какая нравится
 const getData = getDataAsync; // || getDataPromise;
-
-async function loadCountriesData(url = 'https://restcountries.com/v3.1/all?fields=name&fields=cca3&fields=area')
+async function loadCountriesData(url = 'https://restcountries.com/v3.1/all?fields=name&fields=cca3&fields=area'):Promise<{[key:string]:ICountry}>
 {
-    let countries = [];
+    let countries: ICountry[] = [];
     try
     {
         // ПРОВЕРКА ОШИБКИ №1: ломаем этот урл, заменяя all на allolo,
         // получаем кастомную ошибку.
         countries = await getData(url);
     }
-    catch (error)
+    catch (error:unknown)
     {
         console.log(`catch for getData check url ${url}. Start should be https://restcountries.com/v3.1/all`);
         console.error(error);
         throw error;
     }
-    return countries.reduce((result, country) =>
+    return countries.reduce<{ [key:string]: ICountry }>((result, country:ICountry) =>
     {
-        console.log(result);
-        console.log(country);
         result[country.cca3] = country;
         return result;
-    }, {});
+    },{});
 }
-
-function getCountryByCode(code, countriesData)
-{
-    for (const [key, value] of Object.entries(countriesData))
-    {
-        if (code !== '' && code === value?.cca3)
-        {
-            return value.name.common;
-        }
-    }
-    return '';
-}
-function getNameCountryInThePath(paths, countriesData)
-{
-    let path = '';
-    module.setEndPoints(paths[0], paths[paths.length - 1]);
-    for (let i = 0; i < paths.length; i++)
-    {
-        const country = getCountryByCode(paths[i], countriesData);
-        if (i >= 1 && i <= paths.length - 1)
-            module.markAsVisited([paths[i - 1], paths[i]]);
-
-        if (i !== paths.length - 1)
-            path += `${country} -> `;
-        else
-            path += country;
-    }
-    return path;
-}
-function nearbyBroder(borders, endCountry)
-{
-    for (let i = 0; i < borders.length; i++)
-        if (borders[i] === endCountry)
-            return endCountry;
-    return null;
-}
-
-function findRoute(graph, startCountry, endCountry, countReq = 0, visited = new Set(), maxSteps = 10)
-{
-    if (startCountry === endCountry)
-    {
-        totalRequest = countReq;
-        return [startCountry];
-    }
-
-    if (visited.has(startCountry) || maxSteps === 0)
-    {
-        return null;
-    }
-
-    visited.add(startCountry);
-
-    const borders = graph[startCountry]?.borders || [];
-    const endCountryNearbyBorder = nearbyBroder(borders, endCountry);
-    if (endCountryNearbyBorder !== null)
-    {
-        totalRequest = countReq;
-        return [startCountry, endCountry];
-    }
-
-    for (const neighbor of borders)
-    {
-        const result = findRoute(graph, neighbor, endCountry, countReq + 1, new Set(visited), maxSteps - 1);
-        if (result !== null)
-        {
-            return [startCountry, ...result];
-        }
-    }
-
-    return null;
-}
-
-async function calcPath(fromCode, toCode, countriesData)
-{
-    let paths = findRoute(countriesData, fromCode, toCode);
-    console.log(totalRequest);
-    if (paths === null) // is island or other continent
-        paths = [fromCode, toCode];
-    return getNameCountryInThePath(paths, countriesData);
-}
-
-function getCCA3ByNameCountry(from, to, countriesData)
-{
-    const cca3 = {
-        from: '',
-        to: '',
-    };
-    for (const [key, value] of Object.entries(countriesData))
-    {
-        if (cca3.from === '' && from === value?.name.common)
-        {
-            cca3.from = value.cca3;
-        }
-        if (cca3.to === '' && to === value?.name.common)
-        {
-            cca3.to = value.cca3;
-        }
-        if (cca3.to !== '' && cca3.from !== '')
-            return cca3;
-    }
-    return cca3;
-}
-
-export { getCCA3ByNameCountry, calcPath, loadCountriesData, totalRequest };
